f29e734350c35a2eeeab07f00f4901e30018aab6
Laurent GAY <l.gay@sd-libre.fr>
2019-12-11 16:45:22 +0100

    dependancy

--- setup.py
+++ setup.py
@@ -59,5 +59,5 @@ setup(
         "lucterios.documents.migrations": ['*'],
         "lucterios.documents": ['build', 'images/*', 'locale/*/*/*', 'help/*'],
     },
-    install_requires=["lucterios ~=2.4", "Pillow ~=6.0", "etherpad-lite ~=0.5", "requests"],
+    install_requires=["lucterios ~=2.4", "etherpad-lite ~=0.5", "requests"],
 )
